e8a34ebf0d338d9d20b5a168a2bb9190f2b0e4b2
Lorne <dev@luotao.net>
2019-07-02 19:59:39 +1000

    model skeleton for bakery and product

---
+++ bakery.py
@@ -0,0 +1,50 @@
+import logging
+from decimal import Decimal, ROUND_DOWN
+
+
+class Bakery:
+    _products = {}
+
+    def __init__(self, *products):
+        raise NotImplementedError
+
+    def process_order(self, order_dict):
+        raise NotImplementedError
+
+    # expose as property to make sure immutable from outside
+    @property
+    def products(self):
+        return self._products
+
+class Product:
+    _packs = {}
+
+    def __init__(self, name, code, pack_price_dict):
+        raise NotImplementedError
+
+
+    def get_pack_price(self, quantity):
+        return self._packs[quantity]
+
+    # expose as property to make sure immutable from outside
+    @property
+    def packs(self):
+        return self._packs
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def code(self):
+        return self._code
+
+    @property
+    def pack_quantity(self):
+        quantity_options = list(self._packs.keys())
+        quantity_options.sort()
+        return quantity_options
+
+    def pack_order(self,quantity):
+        raise NotImplementedError
+
